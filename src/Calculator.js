0cd37c2fb90c3d0e4c2cc51b863e7ba0369b96d8
Poplica <tu9489@gmail.com>
2021-06-26 23:12:56 -0400

    handleDigit and handleDot finished

--- src/Calculator.js
+++ src/Calculator.js
@@ -5,20 +5,36 @@ import { useEffect, useState } from 'react';
 import { evaluate } from 'mathjs';
 
 export default function Calculator() {
-  const [expression, setExpression] = useState([]);
-  const [lowerVal, setLowerVal] = useState('0'); // input state
-  const [isReadyForResult, setIsReadyForResult] = useState(false);
-  const [prevKey, setPrevKey] = useState(null);
-  const operRegEx = new RegExp('\\+|-|\\*|\\/');
+  const states = {
+    FIRST_ARG: 0,
+    SECOND_ARG: 1,
+    EQUAL: 2
+  }
+
+  const [expression, setExpression] = useState([])
+  const [result, setResult] = useState()
+  const [lowerVal, setLowerVal] = useState('0')
+  const [currState, setCurrState] = useState(states.FIRST_ARG)
+  const [operator, setOperator] = useState(null)
+  const [prevKey, setPrevKey] = useState(null)
+
+  const [isReadyForResult, setIsReadyForResult] = useState(false)
+  const operRegEx = new RegExp('\\+|-|\\*|\\/')
+
+  useEffect(() => {
+
+  }, [expression])
 
   const handleDelete = () => {
-    if (prevKey !== '=' && lowerVal !== '0') {
-      setLowerVal((prev) => {
-        // console.log("lowerVal: " + lowerVal + ", typeof: " + typeof lowerVal)
-        // console.log("prev.length: " + prev.length + ", typeof: " + typeof prev.length)
-        return prev.length === 1 ? '0' : prev.slice(0, -1)
-      })
-    }
+
+
+    // if (prevKey !== '=' && lowerVal !== '0') {
+    //   setLowerVal((prev) => {
+    //     // console.log("lowerVal: " + lowerVal + ", typeof: " + typeof lowerVal)
+    //     // console.log("prev.length: " + prev.length + ", typeof: " + typeof prev.length)
+    //     return prev.length === 1 ? '0' : prev.slice(0, -1)
+    //   })
+    // }
     // setPrevKey('del')
   }
 
@@ -30,101 +46,101 @@ export default function Calculator() {
   }
 
   const handleClearEntry = () => {
-    console.log("asdfasdf:" + expression)
-    setLowerVal('0')
+    // console.log("asdfasdf:" + expression)
+    // setLowerVal('0')
     setPrevKey('CE')
   }
 
   const handleOperator = () => ({target}) => {
     const operator = target.name
 
-    // if prevKey was an operator, replace operator
-    if (operRegEx.test(prevKey)) {
-      setExpression((prev) => { // edge case: operator, dot, equal
-        if (operRegEx.test(prev.slice(-1))) 
-          prev.pop()
-        return [...prev, operator]
-      })
-    } else if (prevKey === '.') { // edge case for dot input
-      setLowerVal((prev) => prev.slice(0, -1))
-      setExpression((prev) => [...prev, lowerVal.slice(0, -1), operator])
-    } else {
-      setExpression((prev) => {
-        if (prevKey === '=')
-          return [...prev, operator]
-        else if (prevKey === 'CE')
-          return [lowerVal, operator]
-        else
-          return [...prev, lowerVal, operator]
-      })
-    }
-    console.log("here@")
-    console.log('prevKey: ' + prevKey)
+    // // if prevKey was an operator, replace operator
+    // if (operRegEx.test(prevKey)) {
+    //   setExpression((prev) => { // edge case: operator, dot, equal
+    //     if (operRegEx.test(prev.slice(-1))) 
+    //       prev.pop()
+    //     return [...prev, operator]
+    //   })
+    // } else if (prevKey === '.') { // edge case for dot input
+    //   setLowerVal((prev) => prev.slice(0, -1))
+    //   setExpression((prev) => [...prev, lowerVal.slice(0, -1), operator])
+    // } else {
+    //   setExpression((prev) => {
+    //     if (prevKey === '=')
+    //       return [...prev, operator]
+    //     else if (prevKey === 'CE')
+    //       return [lowerVal, operator]
+    //     else
+    //       return [...prev, lowerVal, operator]
+    //   })
+    // }
+    // console.log("here@")
+    // console.log('prevKey: ' + prevKey)
 
     setPrevKey(operator)
   }
 
   const handleDot = () => {
     if (!lowerVal.includes('.')) {
-      setLowerVal((prev) => (prev + '.'))
+      if (prevKey === '=' || operRegEx.test(prevKey) || expression.length === 0)
+        setLowerVal('0.')
+      else
+        setLowerVal((prev) => prev + '.')
     }
     setPrevKey('.')
   }
 
   const handleDigit = () => ({target}) => {
     const newDigit = target.name;
-
-    if (operRegEx.test(prevKey)) { // next input should overwrite lowerVal
+      
+    if (prevKey !== '=' && !operRegEx.test(prevKey)) { // overwrite 0. otherwise, append
+      setLowerVal((prev) => prev === '0' ? newDigit : prev + newDigit)
+    } else { // next input overwrites lowerVal
+      if (prevKey === '=')
+        setExpression([])
       setLowerVal(newDigit)
-    // otherwise, if 0, then overwrite otherwise append
-    } else if (prevKey === '=') {
-      setExpression([])
-      setLowerVal(newDigit)
-    } else {
-      setLowerVal((prev) => (
-        prev === '0' ? newDigit : prev + newDigit
-      ));
     }
+
     setPrevKey(newDigit)
   }
 
-  useEffect(() => {
-    // rerenders the display and evaluates the expression when = is pressed
-    if (isReadyForResult) {
-      console.log('expression: ' + expression)
-      setLowerVal(evaluate(expression.join('')) + '')
-      setIsReadyForResult(false)
-      setPrevKey('=')
-    }
-  }, [isReadyForResult, expression])
+  // useEffect(() => {
+  //   // rerenders the display and evaluates the expression when = is pressed
+  //   if (isReadyForResult) {
+  //     console.log('expression: ' + expression)
+  //     setLowerVal(evaluate(expression.join('')) + '')
+  //     setIsReadyForResult(false)
+  //     setPrevKey('=')
+  //   }
+  // }, [isReadyForResult, expression])
 
-  const getOperator = () =>{
-    return expression.slice(-2)[0]
-  }
+  // const getOperator = () =>{
+  //   return expression.slice(-2)[0]
+  // }
 
-  const getOperand = () =>{
-    return expression.slice(-1)[0]
-  }
+  // const getOperand = () =>{
+  //   return expression.slice(-1)[0]
+  // }
 
   const handleEqual = () => () => {
-    console.log('expression2: ' + expression)
+    // console.log('expression2: ' + expression)
     // console.log('prevKey: ' + prevKey)
 
-    if (!operRegEx.test(expression)) {
-      setExpression([lowerVal])
-    } else {
-      if (prevKey === '=') {// repeat last operator and operand
-        setExpression((prev) => [...prev, prev.slice(-2)[0], prev.slice(-1)[0]])
-      } else if (prevKey === 'CE') { // use last operator and operand on curr lowerVal
-        setExpression((prev) => [lowerVal, prev.slice(-2)[0], prev.slice(-1)[0]])
-        console.log("here")
+    // if (!operRegEx.test(expression)) {
+    //   setExpression([lowerVal])
+    // } else {
+    //   if (prevKey === '=') {// repeat last operator and operand
+    //     setExpression((prev) => [...prev, prev.slice(-2)[0], prev.slice(-1)[0]])
+    //   } else if (prevKey === 'CE') { // use last operator and operand on curr lowerVal
+    //     setExpression((prev) => [lowerVal, prev.slice(-2)[0], prev.slice(-1)[0]])
+    //     console.log("here")
 
-      } else { // just append
-        setExpression((prev) => ([...prev, lowerVal]))
-      }
-    }
+    //   } else { // just append
+    //     setExpression((prev) => ([...prev, lowerVal]))
+    //   }
+    // }
 
-    setIsReadyForResult(true)
+    // setIsReadyForResult(true)
     setPrevKey('=')
   }
 
