0e33bf809a9cef6364cf74c60dd03284e4a699ed
Poplica <tu9489@gmail.com>
2021-06-26 20:21:18 -0400

    first test written

--- src/Calculator.test.js
+++ src/Calculator.test.js
@@ -1,12 +1,31 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Calculator from './Calculator';
 
-test('renders learn react link', () => {
-  render(<Calculator />);
-  const button = screen.getByRole('button', {name: '1'})
-  fireEvent.click(button)
-  expect(screen.getByText('1')).toBeInTheDocument()
+function getButton(val) {
+  return screen.getByRole('button', {name: val})
+}
 
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+test('lowerVal operations', () => {
+  render(<Calculator />);
+
+  const expression = screen.getByTestId('expression')
+  const lowerVal = screen.getByTestId('lowerVal')
+
+  fireEvent.click(getButton('1'))
+  expect(lowerVal.textContent).toBe('1')
+
+  fireEvent.click(getButton('1'))
+  expect(lowerVal.textContent).toBe('11')
+
+  fireEvent.click(getButton('+'))
+  fireEvent.click(getButton('3'))
+  fireEvent.click(getButton('='))
+  expect(lowerVal.textContent).toBe('14')
+  expect(expression.textContent).toBe('11+3')
 });
+
+// test('displays: 11', () => {
+//   render(<Calculator />);
+//   const lowerVal = screen.getByTestId('lowerVal')
+//   fireEvent.click(getButton('1'))
+// });
--- src/components/CalcDisplay.js
+++ src/components/CalcDisplay.js
@@ -4,10 +4,10 @@ export default function CalcDisplay({upperVal, lowerVal}) {
   return (
     <div>
       <div className="outer-div">
-        <div className="inner-div sm-text">{upperVal}</div>
+        <div className="inner-div sm-text" data-testid="expression">{upperVal}</div>
       </div>
       <div className="outer-div">
-        <div className="inner-div lg-text">{lowerVal}</div>
+        <div className="inner-div lg-text" data-testid="lowerVal">{lowerVal}</div>
       </div>
     </div>
   )
