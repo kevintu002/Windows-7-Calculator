e5b6b22bca40cbc62bf953a86454db3ffd263197
Poplica <tu9489@gmail.com>
2021-06-27 12:17:22 -0400

    created edge case test for handleOperator

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -5,27 +5,41 @@ function getButton(val) {
   return screen.getByRole('button', {name: val})
 }
 
+function clickButton(val) {
+  fireEvent.click(getButton(val))
+}
+
 test('lowerVal operations', () => {
   render(<Calculator />);
-
   const expression = screen.getByTestId('expression')
   const lowerVal = screen.getByTestId('lowerVal')
 
-  fireEvent.click(getButton('1'))
+  clickButton('1')
   expect(lowerVal.textContent).toBe('1')
 
-  fireEvent.click(getButton('1'))
+  clickButton('1')
   expect(lowerVal.textContent).toBe('11')
 
-  fireEvent.click(getButton('+'))
-  fireEvent.click(getButton('3'))
-  fireEvent.click(getButton('='))
+  clickButton('+');clickButton('3');clickButton('=')
   expect(lowerVal.textContent).toBe('14')
   expect(expression.textContent).toBe('11+3')
 });
 
-// test('displays: 11', () => {
-//   render(<Calculator />);
-//   const lowerVal = screen.getByTestId('lowerVal')
-//   fireEvent.click(getButton('1'))
-// });
+test('dot operator cases', () => {
+  render(<Calculator />);
+  const expression = screen.getByTestId('expression')
+  const lowerVal = screen.getByTestId('lowerVal')
+
+  clickButton('1');clickButton('.')
+  expect(lowerVal.textContent).toBe('1.')
+
+  clickButton('1');clickButton('.');clickButton('+')
+  expect(lowerVal.textContent).toBe('1')
+  expect(expression.textContent).toBe('1+')
+
+  clickButton('C')
+  clickButton('1');clickButton('.');clickButton('1');clickButton('1')
+  clickButton('.');clickButton('+')
+  expect(lowerVal.textContent).toBe('1.11')
+  expect(expression.textContent).toBe('1.11+')
+})
