99bfffef62b97b9728735480f0dacf16a7167996
Poplica <tu9489@gmail.com>
2021-06-28 12:43:48 -0400

    major handleEqual changes
    - handleEqual reworked and fixed a lot of previous code
    
    also added and reworked a lot of tests

--- src/Calculator.js
+++ src/Calculator.js
@@ -20,14 +20,6 @@ export default function Calculator() {
   const [isReadyForResult, setIsReadyForResult] = useState(false)
   const operRegEx = new RegExp('\\+|-|\\*|\\/')
 
-  const handleDelete = () => {
-    if (prevKey !== '=' && lowerVal !== '0') {
-      setLowerVal((prev) => prev.length === 1 ? '0' : prev.slice(0, -1))
-
-      setPrevKey('del')
-    }
-  }
-
   const handleClear = () => {
     setExpression([])
     setLowerVal('0')
@@ -41,36 +33,19 @@ export default function Calculator() {
     setPrevKey('CE')
   }
 
-  const handleOperator = () => ({target}) => {
-    const newOperator = target.name
-    
-    if (operRegEx.test(prevKey)) {
-      // replace operator
-      setExpression((prev) => [...prev.slice(0,-1), newOperator])
-    } else if (prevKey === '.') {
-      // remove dot
-      const newLowerVal = lowerVal.slice(0,-1)
-      setLowerVal(newLowerVal)
-      setExpression((prev) => [...prev, newLowerVal, newOperator])
-    } else if (prevKey === '=') {
-      // append to existing expression
-      setExpression((prev) => [...prev, newOperator])
-    } else if (prevKey === 'CE' && !operRegEx.test(expression)) {
-      // new expression
-      setExpression((_) => [lowerVal, newOperator])
-    } else {
-      // evaluate and display current expression with new operator
-      setLowerVal(evaluate([...expression, lowerVal].join('')) + '')
-      setExpression((prev) => [...prev, lowerVal, newOperator])
-    }
+  const handleDelete = () => {
+    if (prevKey !== '=' && lowerVal !== '0') {
+      setLowerVal((prev) => prev.length === 1 ? '0' : prev.slice(0, -1))
 
-    setPrevKey(newOperator)
+      setPrevKey('del')
+    }
   }
 
   const handleDot = () => {
     if (!lowerVal.includes('.')) {
       if (prevKey === '=' || operRegEx.test(prevKey) 
       || (expression.length === 0 && lowerVal === '0'))
+      // prepend 0 under special cases
         setLowerVal('0.')
       else
         setLowerVal((prev) => prev + '.')
@@ -95,6 +70,34 @@ export default function Calculator() {
     setPrevKey(newDigit)
   }
 
+  const handleOperator = () => ({target}) => {
+    const newOperator = target.name
+    
+    if (operRegEx.test(prevKey)) {
+      // replace operator
+      setExpression((prev) => [...prev.slice(0,-1), newOperator])
+    } else if (prevKey === '.') {
+      // remove dot
+      const newLowerVal = lowerVal.slice(0,-1)
+
+      setExpression((prev) => [...prev, newLowerVal, newOperator])
+      setLowerVal(newLowerVal)
+    } else if (prevKey === '=') {
+      // append to existing expression
+      setExpression((prev) => [...prev, newOperator])
+    } else if (prevKey === 'CE' && !operRegEx.test(expression)) {
+      // new expression
+      setExpression((_) => [lowerVal, newOperator])
+    } else {
+      // evaluate and display current expression with new operator
+      setExpression((prev) => [...prev, lowerVal, newOperator])
+      // setLowerVal(evaluate([...expression, lowerVal].join('')) + '')
+      setLowerVal(myEval([...expression, lowerVal]))
+    }
+
+    setPrevKey(newOperator)
+  }
+
   // useEffect(() => {
   //   // rerenders the display and evaluates the expression when = is pressed
   //   if (isReadyForResult) {
@@ -112,24 +115,54 @@ export default function Calculator() {
   // const getOperand = () =>{
   //   return expression.slice(-1)[0]
   // }
+  const myEval = (stringArr) => {
+    return evaluate(stringArr.join('')) + ''
+  }
 
   const handleEqual = () => () => {
     // console.log('expression2: ' + expression)
     // console.log('prevKey: ' + prevKey)
 
-    // if (!operRegEx.test(expression)) {
-    //   setExpression([lowerVal])
-    // } else {
-    //   if (prevKey === '=') {// repeat last operator and operand
-    //     setExpression((prev) => [...prev, prev.slice(-2)[0], prev.slice(-1)[0]])
-    //   } else if (prevKey === 'CE') { // use last operator and operand on curr lowerVal
-    //     setExpression((prev) => [lowerVal, prev.slice(-2)[0], prev.slice(-1)[0]])
-    //     console.log("here")
+    if (!operRegEx.test(expression)) {
+      setExpression([lowerVal])
+    } else if (prevKey === '.') {
+      // 0+1.=
+      // 0+.=
+      // 0+1.1.=
+      
+      // console.log(`lastOp: ${lastOperator}`)
 
-    //   } else { // just append
-    //     setExpression((prev) => ([...prev, lowerVal]))
-    //   }
-    // }
+      // const newLowerVal = lowerVal
+      // if (!lowerVal.includes('.'))
+      const newLowerVal = lowerVal.slice(0,-1)
+
+      // was last item in expression was an operator?
+      if (operRegEx.test(expression.slice(-1))) {
+        setExpression((prev) => [...prev, newLowerVal])
+        setLowerVal(myEval([...expression, newLowerVal]))
+      } else {
+        const [lastOperator, lastOperand] = expression.slice(-2)
+        setExpression((_) => [newLowerVal, lastOperator, lastOperand])
+        setLowerVal(myEval([newLowerVal, lastOperator, lastOperand]))
+      } 
+    } else if (prevKey === '=') {
+      // repeat last operator and operand
+      const [lastOperator, lastOperand] = expression.slice(-2)
+      const newResult = myEval([...expression, lastOperator, lastOperand])
+
+      setExpression((_) => [lowerVal, lastOperator, lastOperand])
+      setLowerVal(newResult)
+    } else if (prevKey === 'CE') {
+      // use last operator and operand on curr lowerVal
+      const [lastOperator, lastOperand] = expression.slice(-2)
+      const newResult = myEval([lowerVal, lastOperator, lastOperand])
+
+      setExpression((_) => [lowerVal, lastOperator, lastOperand])
+      setLowerVal(newResult)
+    } else { // just append
+      setExpression((_) => ([...expression, lowerVal]))
+      setLowerVal(myEval([...expression, lowerVal]))
+    }
 
     // setIsReadyForResult(true)
     setPrevKey('=')
--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -9,11 +9,18 @@ function clickButton(val) {
   fireEvent.click(getButton(val))
 }
 
+function clickSeriesOfButtons(str) {
+  str.split('').forEach(key => {
+    clickButton(key)
+  });
+}
+
 test('lowerVal operations', () => {
   render(<Calculator />);
   const expression = screen.getByTestId('expression')
   const lowerVal = screen.getByTestId('lowerVal')
 
+  clickButton('C')
   clickButton('1')
   expect(lowerVal.textContent).toBe('1')
 
@@ -21,8 +28,8 @@ test('lowerVal operations', () => {
   expect(lowerVal.textContent).toBe('11')
 
   clickButton('+');clickButton('3');clickButton('=')
-  expect(lowerVal.textContent).toBe('14')
   expect(expression.textContent).toBe('11+3')
+  expect(lowerVal.textContent).toBe('14')
 });
 
 test('dot operator cases', () => {
@@ -30,37 +37,99 @@ test('dot operator cases', () => {
   const expression = screen.getByTestId('expression')
   const lowerVal = screen.getByTestId('lowerVal')
 
+  // 1.+
+  clickButton('C')
   clickButton('1');clickButton('.');clickButton('+')
-  expect(lowerVal.textContent).toBe('1')
   expect(expression.textContent).toBe('1+')
+  expect(lowerVal.textContent).toBe('1')
 
+  // 1.1.+
   clickButton('C')
   clickButton('1');clickButton('.')
-  expect(lowerVal.textContent).toBe('1.')
-
   clickButton('1');clickButton('.');clickButton('+')
-  expect(lowerVal.textContent).toBe('1.1')
   expect(expression.textContent).toBe('1.1+')
+  expect(lowerVal.textContent).toBe('1.1')
 
+  // 1+CE-
   clickButton('C')
   clickButton('1');clickButton('+');clickButton('CE');clickButton('-')
-  expect(lowerVal.textContent).toBe('1')
   expect(expression.textContent).toBe('1+0-')
+  expect(lowerVal.textContent).toBe('1')
 
+  // 1.11.+
   clickButton('C')
   clickButton('1');clickButton('.');clickButton('1');clickButton('1')
   clickButton('.');clickButton('+')
-  expect(lowerVal.textContent).toBe('1.11')
   expect(expression.textContent).toBe('1.11+')
+  expect(lowerVal.textContent).toBe('1.11')
 
+  // +.=
   clickButton('C')
   clickButton('+');clickButton('.');clickButton('=')
-  expect(lowerVal.textContent).toBe('0')
   expect(expression.textContent).toBe('0+0')
+  expect(lowerVal.textContent).toBe('0')
 
+  // 1+2=CE=
   clickButton('C')
   clickButton('1');clickButton('+');clickButton('2');clickButton('=');clickButton('CE');clickButton('=')
-  expect(lowerVal.textContent).toBe('2')
   expect(expression.textContent).toBe('0+2')
+  expect(lowerVal.textContent).toBe('2')
+
+  // 1+2=.=
+  clickButton('C')
+  clickButton('1');clickButton('+');clickButton('2');clickButton('=');clickButton('.');clickButton('=')
+  expect(expression.textContent).toBe('0+2')
+  expect(lowerVal.textContent).toBe('2')
+
+  // 1+.=
+  clickButton('C')
+  clickButton('1');clickButton('+');clickButton('.');clickButton('=');
+  expect(expression.textContent).toBe('1+0')
+  expect(lowerVal.textContent).toBe('1')
+  
+  // 1+.1.=
+  clickButton('C')
+  clickButton('1');clickButton('+');clickButton('.');clickButton('1');clickButton('.');clickButton('=')
+  expect(expression.textContent).toBe('1+0.1')
+  expect(lowerVal.textContent).toBe('1.1')
 })
+
+test('CE cases', () => {
+  render(<Calculator />);
+  const expression = screen.getByTestId('expression')
+  const lowerVal = screen.getByTestId('lowerVal')
+
+  // 1+CE-
+  clickButton('C')
+  clickButton('1');clickButton('+');clickButton('CE');clickButton('-')
+  expect(expression.textContent).toBe('1+0-')
+  expect(lowerVal.textContent).toBe('1')
+
+  // 1+2=CE=
+  clickButton('C')
+  clickButton('1');clickButton('+');clickButton('2');clickButton('=');clickButton('CE');clickButton('=')
+  expect(expression.textContent).toBe('0+2')
+  expect(lowerVal.textContent).toBe('2')
+})
+
 // 1=+
+
+test('handleEqual', () => {
+  render(<Calculator />);
+  const expression = screen.getByTestId('expression')
+  const lowerVal = screen.getByTestId('lowerVal')
+
+  // dot cases
+  // 0+1.=
+  clickSeriesOfButtons('C0+1.=')
+  expect(expression.textContent).toBe('0+1')
+  expect(lowerVal.textContent).toBe('1')
+  // 0+.=
+  clickSeriesOfButtons('C0+.=')
+  expect(expression.textContent).toBe('0+0')
+  expect(lowerVal.textContent).toBe('0')
+  // 0+1.1.=
+  clickSeriesOfButtons('C0+1.1.=')
+  expect(expression.textContent).toBe('0+1.1')
+  expect(lowerVal.textContent).toBe('1.1')
+})
